448638531f763520e344f0e2495dbeec480c8617
Leonardo Barcelos <leobarcelos12@gmail.com>
2018-10-11 17:20:57 -0300

    Melhorado script para inicialização do appinventor

--- appinventor-start.sh
+++ appinventor-start.sh
@@ -4,8 +4,10 @@ PORT=8888
 
 cd ./appinventor
 
-nohup $APP_SERVER --port=$PORT --address=0.0.0.0 ./appengine/build/war & PID_APPINVENTOR=$!
-nohup ant RunLocalBuildServer > ../../buildserver-log.out & PID_BUILDSERVER=$!
+mkdir -p $HOME/appinventor-logs
+
+nohup $APP_SERVER --port=$PORT --address=0.0.0.0 ./appengine/build/war > $HOME/appinventor-logs/prod-server.out & PID_APPINVENTOR=$!
+nohup ant RunLocalBuildServer > $HOME/appinventor-logs/buildserver-log.out & PID_BUILDSERVER=$!
 
 cd ..
 
